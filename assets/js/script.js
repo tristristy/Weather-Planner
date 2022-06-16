5b9d09284b8b3f6be426bd63f9cc5be9a8028213
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-06-16 08:47:42 -0400

    made swap from uikit back to bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,39 @@
-var apiKey = "a14c054b595eba0e2721db026cf78927"
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b480d29e23msh0abb34b9a11b6c1p1c16e4jsn4c7767506acd',
+		'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+	}
+};
 
+var city = $("#cityName").val();
 
-var geoCall = function(){
-    var zipCode = "08085"
-    fetch('http://api.openweathermap.org/geo/1.0/zip?zip=' + zipCode + "&appid=" +  apiKey)
-        .then((data) => data.json())
-        .then((data) => weatherCall(data));
+$("#cityName").keypress(function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        $("#searchButton").click();
+    }
+});
+
+$("#searchButton").on("click", function () {
+    $("#forcastTitle").removeClass("invisible")
+
+    city = $("#cityName").val();
+
+    $("#cityName").val("");
+
+    fetch('https://community-open-weather-map.p.rapidapi.com/weather?q=' + city + '&units=imperial', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     
-
         
-    
-};
+        getConditions(response);
+        getForcast();
+        makeList();
+});
 
-var weatherCall = function(){
-    
+function makeList(){
+    var listItem = $("<li>").text(city);
+    $("#recentSearches").appendChild(listItem);
 };
-
-geoCall();
