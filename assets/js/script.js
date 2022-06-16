bea1d5f3f6ab7fe7cd115fab82d46c79e0ef2a6b
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-06-16 09:10:54 -0400

    added current forcast card

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var city = $("#cityName").val();
 
 var key = "&appid=a14c054b595eba0e2721db026cf78927"
 
+var date = new Date();
+
 $("#cityName").keypress(function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
@@ -9,7 +11,7 @@ $("#cityName").keypress(function (event) {
     }
 });
 
-$("#searchBtn").on("click", function() {
+$("#searchBtn").on("click", function(){
 
     $('#forecastTitle').addClass('show');
 
@@ -23,28 +25,51 @@ $("#searchBtn").on("click", function() {
     $.ajax({
         url: callUrl,
         method: "GET"
-      })
-      .then(function (response){
-    
-        console.log(response)
-    
-        console.log(response.name)
-        console.log(response.weather[0].icon)
-    
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        console.log(Math.floor(tempF))
-    
-        console.log(response.main.humidity)
-    
-        console.log(response.wind.speed)
-        
-        getConditions(response);
-        getForecast();
-        makeList();
-        })
-      });
+    })
+        .then(function (response) {
 
-function makeList() {
+            console.log(response)
+
+            console.log(response.name)
+            console.log(response.weather[0].icon)
+
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            console.log(Math.floor(tempF))
+
+            console.log(response.main.humidity)
+
+            console.log(response.wind.speed)
+
+            getConditions(response);
+            //getForecast();
+            makeList();
+        })
+});
+
+var makeList = function() {
     var listItem = $("<li>").addClass("list-group-item").text(city);
     $(".list").append(listItem);
-  }
+}
+
+var getConditions = function(response){
+    var tempImp = (response.main.temp - 273.15) * 1.8 + 32;
+    tempImp = Math.floor(tempImp)
+
+    $("#currentCity").empty();
+
+    const card = $("<div>").addClass("card");
+    const cardBody = $("<div>").addClass("card-body");
+    const city = $("<h4>").addClass("card-title").text(response.name);
+    const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+    const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempImp + " °F");
+    const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+    const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+    const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+
+    //append to display
+
+    city.append(cityDate, image)
+    cardBody.append(city, temperature, humidity, wind);
+    card.append(cardBody);
+    $("#currentCity").append(card)
+}
